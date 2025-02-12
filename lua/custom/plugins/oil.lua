a033029afbb0598fe4f261cad7bf6ef3b12ccef4
whulland <weston.hullander@gmail.com>
2025-02-12 06:35:41 -0600

    show hidden oil

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -1,11 +1,9 @@
 return {
   'stevearc/oil.nvim',
-  ---@module 'oil'
-  ---@type oil.SetupOpts
-  opts = { showhidden = false },
-  -- Optional dependencies
-  dependencies = { { 'echasnovski/mini.icons', opts = {} } },
-  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-  lazy = false,
+  opts = {
+    view_options = {
+      show_hidden = true, -- Enable hidden files
+    },
+  },
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
 }
