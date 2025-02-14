dc862c376a93004520e90427d37ecf03abab2dc5
whulland <weston.hullander@gmail.com>
2025-02-13 21:10:27 -0600

    cleanup

--- lua/custom/plugins/lsp-config.lua
+++ lua/custom/plugins/lsp-config.lua
@@ -167,10 +167,6 @@ return {
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        pyright = {},
-        rust_analyzer = {},
-        basedpyright = {},
-        ruff = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
