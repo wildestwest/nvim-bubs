5cdf3d31b64486a3d95bb7d8c6c647ab7dc1262a
whulland <weston.hullander@gmail.com>
2025-02-16 16:35:46 -0600

    add cursor

--- lua/custom/plugins/blink.lua
+++ lua/custom/plugins/blink.lua
@@ -1,41 +1,259 @@
+---@diagnostic disable: missing-fields
+---
+
+local kinds = {
+  Array = ' ',
+  Boolean = '󰨙 ',
+  Class = ' ',
+  Codeium = '󰘦 ',
+  Color = ' ',
+  Control = ' ',
+  Collapsed = ' ',
+  Constant = '󰏿 ',
+  Constructor = ' ',
+  Copilot = ' ',
+  Enum = ' ',
+  EnumMember = ' ',
+  Event = ' ',
+  Field = ' ',
+  File = ' ',
+  Folder = ' ',
+  Function = '󰊕 ',
+  Interface = ' ',
+  Key = ' ',
+  Keyword = ' ',
+  Method = '󰊕 ',
+  Module = ' ',
+  Namespace = '󰦮 ',
+  Null = ' ',
+  Number = '󰎠 ',
+  Object = ' ',
+  Operator = ' ',
+  Package = ' ',
+  Property = ' ',
+  Reference = ' ',
+  Snippet = '󱄽 ',
+  String = ' ',
+  Struct = '󰆼 ',
+  Supermaven = ' ',
+  TabNine = '󰏚 ',
+  Text = ' ',
+  TypeParameter = ' ',
+  Unit = ' ',
+  Value = ' ',
+  Variable = '󰀫 ',
+}
+
+function expand(snippet)
+  -- Native sessions don't support nested snippet sessions.
+  -- Always use the top-level session.
+  -- Otherwise, when on the first placeholder and selecting a new completion,
+  -- the nested session will be used instead of the top-level session.
+  -- See: https://github.com/LazyVim/LazyVim/issues/3199
+  local session = vim.snippet.active() and vim.snippet._session or nil
+
+  local ok, err = pcall(vim.snippet.expand, snippet)
+  if not ok then
+    local fixed = snippet_fix(snippet)
+    ok = pcall(vim.snippet.expand, fixed)
+
+    local msg = ok and 'Failed to parse snippet,\nbut was able to fix it automatically.' or ('Failed to parse snippet.\n' .. err)
+  end
+
+  -- Restore top-level session when needed
+  if session then
+    vim.snippet._session = session
+  end
+end
+
+function snippet_fix(snippet)
+  local texts = {} ---@type table<number, string>
+  return snippet_replace(snippet, function(placeholder)
+    texts[placeholder.n] = texts[placeholder.n] or snippet_preview(placeholder.text)
+    return '${' .. placeholder.n .. ':' .. texts[placeholder.n] .. '}'
+  end)
+end
+
+function snippet_replace(snippet, fn)
+  return snippet:gsub('%$%b{}', function(m)
+    local n, name = m:match '^%${(%d+):(.+)}$'
+    return n and fn { n = n, text = name } or m
+  end) or snippet
+end
+
+-- This function resolves nested placeholders in a snippet.
+---@param snippet string
+---@return string
+function snippet_preview(snippet)
+  local ok, parsed = pcall(function()
+    return vim.lsp._snippet_grammar.parse(snippet)
+  end)
+  return ok and tostring(parsed) or snippet_replace(snippet, function(placeholder)
+    return snippet_preview(placeholder.text)
+  end):gsub('%$0', '')
+end
 return {
   {
+    'hrsh7th/nvim-cmp',
+    optional = true,
+    enabled = false,
+  },
+  {
     'saghen/blink.cmp',
-    -- optional: provides snippets for the snippet source
-    dependencies = 'rafamadriz/friendly-snippets',
-
-    -- use a release tag to download pre-built binaries
-    version = '*',
-    -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-    -- build = 'cargo build --release',
-    -- If you use nix, you can build from source using latest nightly rust with:
-    -- build = 'nix run .#build-plugin',
+    version = not vim.g.lazyvim_blink_main and '*',
+    build = vim.g.lazyvim_blink_main and 'cargo build --release',
+    opts_extend = {
+      'sources.completion.enabled_providers',
+      'sources.compat',
+      'sources.default',
+    },
+    dependencies = {
+      'rafamadriz/friendly-snippets',
+      -- add blink.compat to dependencies
+      {
+        'saghen/blink.compat',
+        optional = true, -- make optional so it's only enabled if any extras need it
+        opts = {},
+        version = not vim.g.lazyvim_blink_main and '*',
+      },
+    },
+    event = 'InsertEnter',
 
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
-      -- 'default' for mappings similar to built-in completion
-      -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-      -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-      -- See the full "keymap" documentation for information on defining your own keymap.
-      keymap = { preset = 'default' },
-
+      snippets = {
+        expand = function(snippet, _)
+          return expand(snippet)
+        end,
+      },
       appearance = {
-        -- Sets the fallback highlight groups to nvim-cmp's highlight groups
-        -- Useful for when your theme doesn't support blink.cmp
-        -- Will be removed in a future release
-        use_nvim_cmp_as_default = true,
-        -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-        -- Adjusts spacing to ensure icons are aligned
+        -- sets the fallback highlight groups to nvim-cmp's highlight groups
+        -- useful for when your theme doesn't support blink.cmp
+        -- will be removed in a future release, assuming themes add support
+        use_nvim_cmp_as_default = false,
+        -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- adjusts spacing to ensure icons are aligned
         nerd_font_variant = 'mono',
       },
+      completion = {
+        accept = {
+          -- experimental auto-brackets support
+          auto_brackets = {
+            enabled = true,
+          },
+        },
+        menu = {
+          draw = {
+            treesitter = { 'lsp' },
+          },
+        },
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 200,
+        },
+        ghost_text = {
+          enabled = vim.g.ai_cmp,
+        },
+      },
 
-      -- Default list of enabled providers defined so that you can extend it
-      -- elsewhere in your config, without redefining it, due to `opts_extend`
+      -- experimental signature help support
+      -- signature = { enabled = true },
+
       sources = {
+        -- adding any nvim-cmp sources here will enable them
+        -- with blink.compat
+        compat = {},
         default = { 'lsp', 'path', 'snippets', 'buffer' },
       },
+
+      cmdline = {
+        enabled = false,
+      },
+
+      keymap = {
+        preset = 'enter',
+        ['<C-y>'] = { 'select_and_accept' },
+      },
     },
-    opts_extend = { 'sources.default' },
+    ---@param opts blink.cmp.Config | { sources: { compat: string[] } }
+    config = function(_, opts)
+      -- setup compat sources
+      local enabled = opts.sources.default
+      for _, source in ipairs(opts.sources.compat or {}) do
+        opts.sources.providers[source] = vim.tbl_deep_extend('force', { name = source, module = 'blink.compat.source' }, opts.sources.providers[source] or {})
+        if type(enabled) == 'table' and not vim.tbl_contains(enabled, source) then
+          table.insert(enabled, source)
+        end
+      end
+
+      -- Unset custom prop to pass blink.cmp validation
+      opts.sources.compat = nil
+
+      -- check if we need to override symbol kinds
+      for _, provider in pairs(opts.sources.providers or {}) do
+        ---@cast provider blink.cmp.SourceProviderConfig|{kind?:string}
+        if provider.kind then
+          local CompletionItemKind = require('blink.cmp.types').CompletionItemKind
+          local kind_idx = #CompletionItemKind + 1
+
+          CompletionItemKind[kind_idx] = provider.kind
+          ---@diagnostic disable-next-line: no-unknown
+          CompletionItemKind[provider.kind] = kind_idx
+
+          ---@type fun(ctx: blink.cmp.Context, items: blink.cmp.CompletionItem[]): blink.cmp.CompletionItem[]
+          local transform_items = provider.transform_items
+          ---@param ctx blink.cmp.Context
+          ---@param items blink.cmp.CompletionItem[]
+          provider.transform_items = function(ctx, items)
+            items = transform_items and transform_items(ctx, items) or items
+            for _, item in ipairs(items) do
+              item.kind = kind_idx or item.kind
+            end
+            return items
+          end
+
+          -- Unset custom prop to pass blink.cmp validation
+          provider.kind = nil
+        end
+      end
+
+      require('blink.cmp').setup(opts)
+    end,
+  },
+
+  -- add icons
+  {
+    'saghen/blink.cmp',
+    opts = function(_, opts)
+      opts.appearance = opts.appearance or {}
+      opts.appearance.kind_icons = vim.tbl_extend('force', opts.appearance.kind_icons or {}, kinds)
+    end,
+  },
+
+  -- lazydev
+  {
+    'saghen/blink.cmp',
+    opts = {
+      sources = {
+        -- add lazydev to your completion providers
+        default = { 'lazydev' },
+        providers = {
+          lazydev = {
+            name = 'LazyDev',
+            module = 'lazydev.integrations.blink',
+            score_offset = 100, -- show at a higher priority than lsp
+          },
+        },
+      },
+    },
+  },
+  -- catppuccin support
+  {
+    'catppuccin',
+    optional = true,
+    opts = {
+      integrations = { blink_cmp = true },
+    },
   },
 }
--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -1,3 +1,5 @@
+-- these are based on the home row for me as I use the engramm keyboard layout.
+
 return {
   {
     'ThePrimeagen/harpoon',
@@ -8,7 +10,7 @@ return {
 
       harpoon:setup()
 
-      vim.keymap.set('n', '<leader>a', function()
+      vim.keymap.set('n', '<leader>-', function()
         harpoon:list():add()
       end, { desc = 'harpoon add' })
       vim.keymap.set('n', '<leader><C-,>', function()
